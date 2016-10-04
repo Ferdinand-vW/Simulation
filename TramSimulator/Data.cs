2c8299c40ac1b2ab39dbc1119178f9b9017791fd
rogierhans <rogierhans@gmail.com>
2016-10-04 20:54:38 +0200

    Data aangemaakt en een klein testje gemaakt. dag + time + station geeft feq

---
+++ TramSimulator/Data.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TramSimulator
+{
+    class Data
+    {
+        Dictionary<DayOfWeek, DayData> days;
+        public Data()
+        {
+            days = new Dictionary<DayOfWeek, DayData>();
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                days[day] = new DayData();
+            }
+        }
+        public void AddPC(PassengerCount pc)
+        {
+            days[pc.Date.DayOfWeek].AddPC(pc);
+        }
+
+        public double EnteringFQ(DayOfWeek day, String station, double time)
+        {
+            return days[day].EnteringFQ(station, time);
+        }
+        class DayData
+        {
+            Dictionary<int, Min15Block> blocks;
+
+            public DayData()
+            {
+                blocks = new Dictionary<int, Min15Block>();
+            }
+            public double EnteringFQ(String station, double time)
+            {
+                int min = (int)time / 60;
+                min = min - (min % 15);
+                return blocks[min].EnteringFQ(station);
+            }
+
+            public void AddPC(PassengerCount pc)
+            {
+                int min = pc.Time.Hour * 60 + pc.Time.Minute;
+                min = min - (min % 15);
+                if (blocks.ContainsKey(min))
+                {
+                    blocks[min].AddPC(pc);
+                }
+                else
+                {
+                    blocks[min] = new Min15Block();
+                    blocks[min].AddPC(pc);
+                }
+            }
+            class Min15Block
+            {
+                List<PassengerCount> PCs;
+                public Min15Block()
+                {
+                    PCs = new List<PassengerCount>();
+                }
+                public void AddPC(PassengerCount pc)
+                {
+                    PCs.Add(pc);
+                }
+                public double EnteringFQ(String station)
+                {
+                    double feq = 0;
+                    foreach (PassengerCount pc in PCs)
+                        feq += pc.EnteringCounts[station];
+                    if (PCs.Count > 0)
+                        feq = feq / (double)PCs.Count;
+                    return feq;
+                }
+            }
+        }
+    }
+}
--- TramSimulator/Program.cs
+++ TramSimulator/Program.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 using TramSimulator.Events;
 using TramSimulator.States;
@@ -19,32 +19,52 @@ namespace TramSimulator
 
 
         {
-            Simulation sim = new Simulation(null);
-            sim.run(2, null, "monday", new string[] {"Central", "AZR", "PR"});
-            return;
-            
+            if (false)
+            {
+                Simulation sim = new Simulation(null);
+                sim.run(2, null, "monday", new string[] { "Central", "AZR", "PR" });
+                return;
+            }
+
 
             //We probably want a different method of input for the file paths
-            Console.WriteLine("Enter a file path");
-            String path = Console.ReadLine();
+            //Console.WriteLine("Enter a file path");
+            String patha = "F:\\Users\\Rogier\\Desktop\\12a.csv";
+            String pathb = "F:\\Users\\Rogier\\Desktop\\12b.csv";
 
-            Stream stream = File.Open(path, FileMode.Open);
-            List<PassengerCount> passengerCountsA = Parser.ParsePassengerCounts(stream);
+            Stream streama = File.Open(patha, FileMode.Open);
+            Stream streamb = File.Open(pathb, FileMode.Open);
 
+            List<PassengerCount> passengerCountsA = null;
+            List<PassengerCount> passengerCountsB = null;
 
+            var threadA = new Thread(() => { passengerCountsA = Parser.ParsePassengerCounts(streama); });
+            threadA.Start();
+            var threadB = new Thread(() => { passengerCountsB = Parser.ParsePassengerCounts(streamb); });
+            threadB.Start();
+            threadA.Join();
+            threadB.Join();
+
+            Data a = new Data();
+            foreach (PassengerCount pc in passengerCountsA)
+                a.AddPC(pc);
+            Console.WriteLine( a.EnteringFQ(DayOfWeek.Monday, "Heidelberglaan", 9 * 60 * 60));
+            Console.WriteLine(a.EnteringFQ(DayOfWeek.Monday, "Heidelberglaan", 14 * 60 * 60));
+            Console.WriteLine(a.EnteringFQ(DayOfWeek.Tuesday, "Heidelberglaan", 14 * 60 * 60));
+            Console.WriteLine(a.EnteringFQ(DayOfWeek.Monday, "Rubenslaan", 12 * 60 * 60));
             //Some example output
-            Console.WriteLine("Number of passengercounts: " + passengerCountsA.Count);
-            Console.WriteLine(passengerCountsA[0].Trip);
-            Console.WriteLine(passengerCountsA[0].Date);
-            Console.WriteLine(passengerCountsA[0].Time);
-            foreach(var kvp in passengerCountsA[0].EnteringCounts)
-            {
-                Console.WriteLine(kvp.Key + ": " + kvp.Value);
-            }
-            foreach (var kvp in passengerCountsA[0].DepartingCounts)
-            {
-                Console.WriteLine(kvp.Key + ": " + kvp.Value);
-            }
+            //Console.WriteLine("Number of passengercounts: " + passengerCountsA.Count);
+            //Console.WriteLine(passengerCountsA[0].Trip);
+            //Console.WriteLine(passengerCountsA[0].Date);
+            //Console.WriteLine(passengerCountsA[0].Time.Hour);
+            //foreach (var kvp in passengerCountsA[0].EnteringCounts)
+            //{
+            //    Console.WriteLine(kvp.Key + ": " + kvp.Value);
+            //}
+            //foreach (var kvp in passengerCountsA[0].DepartingCounts)
+            //{
+            //    Console.WriteLine(kvp.Key + ": " + kvp.Value);
+            //}
             Console.ReadLine();
         }
     }
